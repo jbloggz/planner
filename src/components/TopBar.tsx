69a37ff6cae7f0c8e3bba08a5891fca6f4f7df0f
Josef Barnes <joe@barnesmail.net>
2025-02-05 00:05:56 +1000

    Fixes path to icon in TopBar component

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from './ThemeToggle';
 function TopBar() {
   return (
     <div className="navbar bg-base-300 flex">
-      <img className="w-7 mr-4 ml-2" src="/planner.png" alt="logo" />
+      <img className="w-7 mr-4 ml-2" src="/favicon-192x192.png" alt="logo" />
       <h1 className="text-xl font-semibold grow">Planner</h1>
       <ThemeToggle lightTheme="light" darkTheme="dark" className="float-right" />
     </div>
